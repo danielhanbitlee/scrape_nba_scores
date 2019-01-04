09a1e3a17416b156bae4c8b65c2d1a877bcc1c89
danielhanbitlee <danielhanbitlee@gmail.com>
2019-01-03 16:58:07 -0800

    updated collection name in pipelines.py

--- nba_scores/pipelines.py
+++ nba_scores/pipelines.py
@@ -24,7 +24,7 @@ class MongoDbPipeline(object):
     def open_spider(self, spider):
         self.client = MongoClient(self.mongo_uri)
         self.db = self.client[self.mongo_db]
-        self.collection = spider.city
+        self.collection = spider.collection
 
     def close_spider(self, spider):
         self.client.close()
