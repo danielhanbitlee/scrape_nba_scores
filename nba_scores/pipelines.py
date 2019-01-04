07a479f88681c5bd96e5322b347e293c6d0b2e96
danielhanbitlee <danielhanbitlee@gmail.com>
2019-01-03 16:33:06 -0800

    filter duplicated data

--- nba_scores/pipelines.py
+++ nba_scores/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from pymongo import MongoClient
-
+from scrapy.exceptions import DropItem
 
 class MongoDbPipeline(object):
     collection = ''
@@ -30,5 +30,19 @@ class MongoDbPipeline(object):
         self.client.close()
 
     def process_item(self, item, spider):
-        self.db[self.collection].insert_one(dict(item))
-        return item
+        if self.db[self.collection].count_documents({'$and': [
+            {'date': item.get('date')},
+            {'team_away_city': item.get('team_away_city')},
+            {'team_away_name': item.get('team_away_name')},
+            {'team_away_score': item.get('team_away_score')},
+            {'team_home_city': item.get('team_home_city')},
+            {'team_home_name': item.get('team_home_name')},
+            {'team_home_score': item.get('team_home_score')}
+            ]
+            }
+            ) == 1:
+            raise DropItem("Item dropped")
+
+        else:
+            self.db[self.collection].insert_one(dict(item))
+            return item
